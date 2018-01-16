b367e6e5aac00c92b693b6a1d168329761a9b313
Unknown <esteban.llanos@correounivalle.edu.co>
2018-01-16 09:03:26 -0500

    Auto stash before merge of "master" and "origin/master"
    Ajustes en la base de datos (TABLA VENTAS y TABLA CIUDAD)

--- KDD/bd_previser.sql
+++ KDD/bd_previser.sql
@@ -11,12 +11,16 @@ DROP TABLE IF EXISTS sede;
 /* Dimensión: Ciudad                                            */
 /*==============================================================*/
 
+CREATE SEQUENCE seq_ciudad INCREMENT BY 1 START WITH 1;
+
 create table ciudad
 (
+  id_ciudad BIGINT NOT NULL DEFAULT nextval('seq_ciudad'::regclass),
   cod_ciudad VARCHAR(5) NOT NULL,
   nombre_ciudad VARCHAR(50) NOT NULL,
   departamento_ciudad VARCHAR(50) NOT NULL,
-  PRIMARY KEY (cod_ciudad, nombre_ciudad)
+  PRIMARY KEY (id_ciudad),
+  UNIQUE (cod_ciudad, nombre_ciudad)
 );
 
 /*==============================================================*/
@@ -25,7 +29,7 @@ create table ciudad
 
 create table plan
 (
-  id_plan VARCHAR(5) NOT NULL,
+  id_plan BIGINT NOT NULL,
   nombre_plan VARCHAR(50) NOT NULL,
   tipo_plan VARCHAR(50) NOT NULL,
   PRIMARY KEY (id_plan) 
@@ -37,7 +41,7 @@ create table plan
 
 create table personal 
 (
-  id_personal VARCHAR(5) NOT NULL,
+  id_personal BIGINT NOT NULL,
   nombre_personal VARCHAR(50) NOT NULL,
   apellido_personal VARCHAR(50) NOT NULL,
   tipo_personal VARCHAR(2) NOT NULL,
@@ -50,7 +54,7 @@ create table personal
 
 create table sede 
 (
-  id_sede VARCHAR(2) NOT NULL DEFAULT 0,
+  id_sede BIGINT NOT NULL DEFAULT 0,
   nombre_sede VARCHAR(50) NOT NULL,
   tipo_sede VARCHAR(5) NOT NULL,
   PRIMARY KEY (id_sede) 
@@ -67,14 +71,14 @@ create table ventas
   id_venta BIGINT NOT NULL DEFAULT nextval('seq_venta'::regclass),
   anio_venta VARCHAR(50) NOT NULL,
   mes_venta VARCHAR(50) NOT NULL,
-  plan_venta VARCHAR(50) NOT NULL,
-  sede_venta VARCHAR(50) NOT NULL,
-  ciudad_venta VARCHAR(50) NOT NULL,
-  vendedor VARCHAR(50) NOT NULL,
+  plan_venta BIGINT NOT NULL,
+  sede_venta BIGINT NOT NULL,
+  ciudad_venta BIGINT NOT NULL,
+  vendedor BIGINT NOT NULL,
   total_ventas INTEGER NOT NULL,
   PRIMARY KEY (id_venta),
   FOREIGN KEY (plan_venta) REFERENCES plan (id_plan),
   FOREIGN KEY (sede_venta) REFERENCES sede (id_sede),
-  FOREIGN KEY (ciudad_venta) REFERENCES ciudad (cod_ciudad),
+  FOREIGN KEY (ciudad_venta) REFERENCES ciudad (id_ciudad),
   FOREIGN KEY (vendedor) REFERENCES personal (id_personal)
 );
